fe4b0788bbf1e3597a178ecc0187322e15a74e0f
Barend Erasmus <hirebarend@gmail.com>
2023-06-18 11:10:09 +0200

    add readme and mixpanel

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Simple Clock using Vite
 
 ![](/public/screenshot.png)
+
+Clocks are timeless (pun intended) tools that are often used as simple and practical examples when learning new technologies. In this repository, we leverage the power of Vite, React, TypeScript, and Bootstrap to create a straightforward clock application.
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import mixpanel from "mixpanel-browser";
 import moment from "moment";
 
 function App() {
   const [date, setDate] = useState(new Date().getTime());
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === "development") {
+      return;
+    }
+
+    mixpanel.init("6570fb6b55412e8145762b070dd25c3b");
+
+    mixpanel.track("Page View");
+  }, []);
 
   setInterval(() => {
     setDate(new Date().getTime());
@@ -11,14 +22,18 @@ function App() {
   return (
     <div className="align-items-center container d-flex h-100 justify-content-center">
       <div className="d-flex display-1 fw-semibold gap-1 gap-md-3">
-        <div className="p-1 p-md-3 text-primary">{moment(date).format("HH")}</div>
-        <div className="p-1 py-md-3 text-white">:</div>
-        <div className="p-1 p-md-3 text-primary">{moment(date).format("mm")}</div>
-        <div className="p-1 py-md-3 text-white">:</div>
-        <div className="p-1 p-md-3 text-primary">{moment(date).format("ss")}</div>
-        <div className="p-1 p-md-3 text-white">
-          {moment(date).format("A")}
+        <div className="p-1 p-md-3 text-primary">
+          {moment(date).format("HH")}
         </div>
+        <div className="p-1 py-md-3 text-white">:</div>
+        <div className="p-1 p-md-3 text-primary">
+          {moment(date).format("mm")}
+        </div>
+        <div className="p-1 py-md-3 text-white">:</div>
+        <div className="p-1 p-md-3 text-primary">
+          {moment(date).format("ss")}
+        </div>
+        <div className="p-1 p-md-3 text-white">{moment(date).format("A")}</div>
       </div>
     </div>
   );
